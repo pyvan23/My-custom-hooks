97e258acb5c57787c295008b76acd5442f3abc0f
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-07-08 20:09:23 -0300

    readme

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,18 @@
+export const todoReducer = (initialState, action) => {
+  switch (action.type) {
+    case "[TODO] add Todo":
+      return [...initialState, action.payload];
+    case "[TODO] remove Todo":
+      return initialState.filter((todo) => todo.id !== action.payload);
+    case "[TODO] toggle Todo":
+      return initialState.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, done: !todo.done };
+        }
+
+        return todo;
+      });
+    default:
+      return initialState;
+  }
+};
